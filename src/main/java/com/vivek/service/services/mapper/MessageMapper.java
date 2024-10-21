4ee823c72955e0e219d5b716f735d94df2f26483
vivekjava <vivekjava96@gmail.com>
2024-10-21 23:38:57 +0530

    Swagger

--- src/main/java/com/vivek/service/services/mapper/MessageMapper.java
+++ src/main/java/com/vivek/service/services/mapper/MessageMapper.java
@@ -3,8 +3,10 @@ package com.vivek.service.services.mapper;
 import com.vivek.service.services.model.RequestMessage;
 import com.vivek.service.services.model.ResponseMessage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface MessageMapper {
+    @Mapping(target = "secretMessage",ignore = true)
     ResponseMessage mapResponse(RequestMessage requestMessage);
 }
